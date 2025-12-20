f08c750370ae71c3365e19f9dc49e234a49be05d
maitree11 <maitreekandpal@gmail.com>
2025-12-20 19:03:58 +0530

    factorial

---
+++ factorial.c
@@ -0,0 +1,18 @@
+//Q29: Write a program to calculate the factorial of a number.
+
+#include <stdio.h>
+int main() {
+
+int n, fact = 1;
+
+printf("enter a number:"); 
+scanf("%d", &n);
+
+for (int i = 1; i <= n; i++) {
+  fact *= i;
+}   
+
+printf("factorial of %d is %d\n", n, fact);
+
+return 0;
+}
