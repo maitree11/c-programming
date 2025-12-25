35c622250777bcfff3321e1481b4f3712eee6264
maitree11 <maitreekandpal@gmail.com>
2025-12-25 19:29:00 +0530

    sum of series

---
+++ sumofseries.c
@@ -0,0 +1,21 @@
+//Q44  Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0.0;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        float numerator = 2 * i - 1;
+        float denominator = 2 * i;
+        sum += numerator / denominator;
+    }
+
+    printf("Approximate sum: %.1f\n", sum);
+
+    return 0;
+}
